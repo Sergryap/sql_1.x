e036c23c76a31c55896f78362b7bdab761e8728f
Sergey Ryapin <rs1180@mail.ru>
2022-06-19 17:55:51 +0500

    Initial commit

---
+++ SQL 1.2_ryapinsergey.sql
@@ -0,0 +1,136 @@
+﻿--=============== МОДУЛЬ 2. РАБОТА С БАЗАМИ ДАННЫХ =======================================
+--= ПОМНИТЕ, ЧТО НЕОБХОДИМО УСТАНОВИТЬ ВЕРНОЕ СОЕДИНЕНИЕ И ВЫБРАТЬ СХЕМУ PUBLIC===========
+SET search_path TO public;
+
+--======== ОСНОВНАЯ ЧАСТЬ ==============
+
+--ЗАДАНИЕ №1
+--Выведите уникальные названия городов из таблицы городов.
+SELECT DISTINCT city
+FROM city
+ORDER BY city;
+
+
+
+
+
+--ЗАДАНИЕ №2
+--Доработайте запрос из предыдущего задания, чтобы запрос выводил только те города,
+--названия которых начинаются на “L” и заканчиваются на “a”, и названия не содержат пробелов.
+SELECT DISTINCT city
+FROM city
+WHERE city iLIKE 'L%a' AND city NOT iLIKE '% %'
+ORDER BY city;
+
+
+
+
+
+--ЗАДАНИЕ №3
+--Получите из таблицы платежей за прокат фильмов информацию по платежам, которые выполнялись 
+--в промежуток с 17 июня 2005 года по 19 июня 2005 года включительно, 
+--и стоимость которых превышает 1.00.
+--Платежи нужно отсортировать по дате платежа.
+SELECT * FROM payment
+WHERE payment_date BETWEEN '17/06/2005'::date AND '19/06/2005'::date
+AND amount <= 1
+ORDER BY payment_date;
+
+
+
+
+
+--ЗАДАНИЕ №4
+-- Выведите информацию о 10-ти последних платежах за прокат фильмов.
+SELECT * FROM payment
+ORDER BY payment_date DESC
+LIMIT 10;
+
+
+
+
+
+--ЗАДАНИЕ №5
+--Выведите следующую информацию по покупателям:
+--  1. Фамилия и имя (в одной колонке через пробел)
+--  2. Электронная почта
+--  3. Длину значения поля email
+--  4. Дату последнего обновления записи о покупателе (без времени)
+--Каждой колонке задайте наименование на русском языке.
+SELECT CONCAT(first_name, ' ', last_name) "Фамили и имя", email "Электронная почта",
+CHARACTER_LENGTH(email) "Длина email", last_update "Дата обновления"
+FROM customer;
+
+
+
+
+
+--ЗАДАНИЕ №6
+--Выведите одним запросом только активных покупателей, имена которых KELLY или WILLIE.
+--Все буквы в фамилии и имени из верхнего регистра должны быть переведены в нижний регистр.
+SELECT customer_id, store_id, LOWER(first_name) irst_name, LOWER(last_name) last_name, email,
+address_id, activebool, create_date, last_update, active
+FROM customer
+WHERE active = 1 AND first_name IN('KELLY', 'WILLIE');
+
+
+
+
+
+--======== ДОПОЛНИТЕЛЬНАЯ ЧАСТЬ ==============
+
+--ЗАДАНИЕ №1
+--Выведите одним запросом информацию о фильмах, у которых рейтинг "R" 
+--и стоимость аренды указана от 0.00 до 3.00 включительно, 
+--а также фильмы c рейтингом "PG-13" и стоимостью аренды больше или равной 4.00.
+SELECT * FROM film
+WHERE (rating = 'R' AND rental_rate BETWEEN 0 AND 3)
+OR (rating = 'PG-13' AND rental_rate >= 4);
+
+
+
+
+
+--ЗАДАНИЕ №2
+--Получите информацию о трёх фильмах с самым длинным описанием фильма.
+SELECT * FROM film
+WHERE CHARACTER_LENGTH(description) IN (
+	SELECT CHARACTER_LENGTH(description) lenght FROM film
+	ORDER BY lenght DESC
+	LIMIT 3)
+ORDER BY title;
+
+
+
+
+
+--ЗАДАНИЕ №3
+-- Выведите Email каждого покупателя, разделив значение Email на 2 отдельных колонки:
+--в первой колонке должно быть значение, указанное до @, 
+--во второй колонке должно быть значение, указанное после @.
+SELECT split_part(email, '@', 1) "До @", split_part(email, '@', 2) "После @"
+FROM customer;
+
+
+
+
+
+--ЗАДАНИЕ №4
+--Доработайте запрос из предыдущего задания, скорректируйте значения в новых колонках: 
+--первая буква должна быть заглавной, остальные строчными.
+
+SELECT upper(substring(split_part(email, '@', 1), '(^.)'))||
+	   OVERLAY(split_part(email, '@', 1) placing
+	   lower(SUBSTRING(split_part(email, '@', 1) from 2 for CHARACTER_LENGTH(split_part(email, '@', 1))))
+	   from 1 for CHARACTER_LENGTH(split_part(email, '@', 1))
+	   ) "До @",
+	   upper(substring(split_part(email, '@', 2), '(^.)'))||
+	   OVERLAY(split_part(email, '@', 1) placing
+	   lower(SUBSTRING(split_part(email, '@', 2) from 2 for CHARACTER_LENGTH(split_part(email, '@', 2))))
+	   from 1 for CHARACTER_LENGTH(split_part(email, '@', 2))
+	   ) "После @"
+FROM customer;
+
+
+
+
---
+++ SQL 1.3_ryapinsergey.sql
@@ -0,0 +1,174 @@
+--=============== МОДУЛЬ 3. ОСНОВЫ SQL =======================================
+--= ПОМНИТЕ, ЧТО НЕОБХОДИМО УСТАНОВИТЬ ВЕРНОЕ СОЕДИНЕНИЕ И ВЫБРАТЬ СХЕМУ PUBLIC===========
+SET search_path TO public;
+
+--======== ОСНОВНАЯ ЧАСТЬ ==============
+
+--ЗАДАНИЕ №1
+--Выведите для каждого покупателя его адрес проживания, 
+--город и страну проживания.
+SELECT customer_id, first_name, last_name, address, city, country
+FROM 
+	customer 
+	JOIN address USING (address_id)
+	JOIN city USING (city_id)
+	JOIN country USING (country_id)
+ORDER BY 1;
+
+
+
+
+
+--ЗАДАНИЕ №2
+--С помощью SQL-запроса посчитайте для каждого магазина количество его покупателей.
+SELECT store_id, COUNT(customer_id) "Покупателей"  
+FROM 
+	store
+	JOIN customer USING (store_id)
+GROUP BY store_id
+ORDER BY store_id;
+
+
+
+
+
+--Доработайте запрос и выведите только те магазины, 
+--у которых количество покупателей больше 300-от.
+--Для решения используйте фильтрацию по сгруппированным строкам 
+--с использованием функции агрегации.
+SELECT store_id, COUNT(customer_id) "Покупателей"  
+FROM 
+	store
+	JOIN	customer USING (store_id)
+GROUP BY	store_id
+HAVING COUNT(customer_id) >300
+ORDER BY store_id;
+
+
+
+
+
+-- Доработайте запрос, добавив в него информацию о городе магазина, 
+--а также фамилию и имя продавца, который работает в этом магазине.
+SELECT s.store_id, s.c "Покупателей", city "Город",
+	   staff.first_name "Имя менеджера", staff.last_name "Фамилия менеджера"
+FROM
+	(SELECT store_id, COUNT(customer_id) c, store.address_id a, store.manager_staff_id m
+	 FROM store
+	 JOIN	customer USING (store_id)
+	 GROUP BY	store_id
+	 HAVING COUNT(customer_id) >300
+	 ) s
+JOIN staff ON s.m = staff.staff_id
+JOIN address ON address.address_id = s.a
+JOIN city USING (city_id);
+
+
+
+
+
+--ЗАДАНИЕ №3
+--Выведите ТОП-5 покупателей, 
+--которые взяли в аренду за всё время наибольшее количество фильмов
+SELECT customer_id, first_name, last_name, count(*) "Количество аренд"
+FROM customer 
+JOIN rental USING (customer_id)
+GROUP BY customer_id 
+ORDER BY count(*)
+DESC LIMIT 5;
+
+
+
+
+
+--ЗАДАНИЕ №4
+--Посчитайте для каждого покупателя 4 аналитических показателя:
+--  1. количество фильмов, которые он взял в аренду
+--  2. общую стоимость платежей за аренду всех фильмов (значение округлите до целого числа)
+--  3. минимальное значение платежа за аренду фильма
+--  4. максимальное значение платежа за аренду фильма
+SELECT customer.customer_id, count(*) "Количество фильмов",
+sum(amount) "Общая сумма", min(amount) "Минимальный платеж", max(amount) "Максимальный платеж"
+FROM customer
+JOIN rental ON customer.customer_id  = rental.customer_id 
+JOIN payment USING (rental_id)
+GROUP BY customer.customer_id 
+ORDER BY 3 DESC;
+
+
+
+
+
+--ЗАДАНИЕ №5
+--Используя данные из таблицы городов составьте одним запросом всевозможные пары городов таким образом,
+ --чтобы в результате не было пар с одинаковыми названиями городов. 
+ --Для решения необходимо использовать декартово произведение.
+SELECT DISTINCT city.city, c.city
+FROM city
+CROSS JOIN (SELECT city FROM city) c;
+ 
+
+
+
+
+--ЗАДАНИЕ №6
+--Используя данные из таблицы rental о дате выдачи фильма в аренду (поле rental_date)
+--и дате возврата фильма (поле return_date), 
+--вычислите для каждого покупателя среднее количество дней, за которые покупатель возвращает фильмы.
+SELECT customer_id, first_name||' ' ||last_name ФИО,
+round(AVG(return_date::date - rental_date::date), 2) "Средняя продолжительность, дней"
+FROM customer
+JOIN rental USING (customer_id)
+GROUP BY customer_id
+ORDER BY ФИО; 
+
+
+
+
+--======== ДОПОЛНИТЕЛЬНАЯ ЧАСТЬ ==============
+
+--ЗАДАНИЕ №1
+--Посчитайте для каждого фильма сколько раз его брали в аренду и значение общей стоимости аренды фильма за всё время.
+SELECT film_id, count(*) Количество, round(sum(amount), 1) Сумма
+FROM film
+	JOIN inventory USING (film_id)
+	JOIN rental USING (inventory_id)
+	JOIN payment USING (rental_id)
+GROUP BY film_id
+ORDER BY 3 DESC;
+
+
+
+
+--ЗАДАНИЕ №2
+--Доработайте запрос из предыдущего задания и выведите с помощью запроса фильмы, которые ни разу не брали в аренду.
+SELECT film_id, title
+FROM film
+	LEFT JOIN inventory USING (film_id)
+	LEFT JOIN rental USING (inventory_id)
+WHERE rental_id IS NULL
+ORDER BY 1;
+
+
+
+
+--ЗАДАНИЕ №3
+--Посчитайте количество продаж, выполненных каждым продавцом. Добавьте вычисляемую колонку "Премия".
+--Если количество продаж превышает 7300, то значение в колонке будет "Да", иначе должно быть значение "Нет".
+SELECT staff.staff_id, count(*),
+	CASE 
+		WHEN count(*) > 7301 THEN 'Да'
+		ELSE 'Нет'
+	END "Премия"
+FROM staff
+	JOIN store ON staff.staff_id = store.manager_staff_id 
+	JOIN customer ON store.store_id = customer.store_id 
+	JOIN payment USING (customer_id)
+GROUP BY 1;
+
+
+
+
+
+
+
